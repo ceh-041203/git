49847ee60e765614786fbffba0e071fe4438df9b
迟恩惠 <2416168610@qq.com>
2025-06-28 11:03:29 +0800

    2.数据预处理

--- Week1.py
+++ Week1.py
@@ -2,3 +2,5 @@ import pandas as pd
 # 检查数据
 df = pd.read_csv('nigerian-songs.csv')
 print('数据基本信息：',df.info())
+# 数据预处理，选择所有数字特征
+X = df.drop(columns=['name', 'album', 'artist', 'artist_top_genre'])
