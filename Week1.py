cdd96869f5227412f353f98e9382eb3d772dbc4c
迟恩惠 <2416168610@qq.com>
2025-06-28 11:07:44 +0800

    3.分析k-means的聚类效果

--- Week1.py
+++ Week1.py
@@ -2,5 +2,24 @@ import pandas as pd
 # 检查数据
 df = pd.read_csv('nigerian-songs.csv')
 print('数据基本信息：',df.info())
+
 # 数据预处理，选择所有数字特征
 X = df.drop(columns=['name', 'album', 'artist', 'artist_top_genre'])
+
+# 分析k-means的聚类效果
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+# 使用轮廓系数选择最佳的簇数
+silhouette_scores = []
+for n_cluster in range(2, 16):
+    kmeans = KMeans(n_clusters=n_cluster)
+    kmeans.fit(X)
+    labels = kmeans.labels_
+    silhouette_scores.append([n_cluster, silhouette_score(X, labels)])
+
+silhouette_scores = pd.DataFrame(silhouette_scores, columns=['簇数', '轮廓系数'])
+best_n_cluster = silhouette_scores[silhouette_scores['轮廓系数'] == silhouette_scores['轮廓系数'].max()]['簇数'].values[0]
+
+print('不同簇数对应的轮廓系数：')
+print(silhouette_scores)
+print(f'最佳簇数: {best_n_cluster}')
